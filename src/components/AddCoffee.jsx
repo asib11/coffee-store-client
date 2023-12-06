d102a81f52dc109868da94408f2574a4d13077ff
asib ahmed <ahmedasib1@gmail.com>
2023-12-07 00:19:26 +0600

    fetch data

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,6 +1,33 @@
 
 
 const AddCoffee = () => {
+    const handleAddCoffee = event =>{
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const chef = form.chef.value;
+        const supplier = form.supplier.value;
+        const taste = form.taste.value;
+        const category = form.category.value;
+        const details = form.details.value;
+        const photo = form.photo.value;
+        const newCoffee = {name, chef, supplier, taste, category, details, photo}
+        console.log(newCoffee);
+        // form.reset();
+        fetch('http://localhost:5000/coffee',{
+            method: 'POST',
+            headers:{
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newCoffee)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
+    }
+
+
     return (
         <div >
             <div className="bg-[#F4F3F0] p-24 ">
@@ -8,7 +35,7 @@ const AddCoffee = () => {
                 <p>
                     It is a long established fact that a reader will be distraceted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here.
                 </p>
-                <form>
+                <form onSubmit={handleAddCoffee}>
                     <div className="md:flex mb-6 ">
                         <label className="form-control w-full">
                             <div className="label">
